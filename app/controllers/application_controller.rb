97353950598542ee8cebe7964bb36b1f9ef74298
Shambhavi Sachid <shambu1985@gmail.com>
2018-03-30 12:55:21 -0700

    Refactored by moving code from application controller into a UpdateUserAgent concern

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
-  before_action :add_or_update_user_agent
-
-  private
-  def add_or_update_user_agent
-    # finds the user agent from database or creates one
-    user_agent = UserAgent.find_or_create_by(name: request.user_agent)
-    user_agent.increment!(:visits_count) # increments visits_count field by 1
-    user_agent.touch # updates updated_at field
-  end
+  include UpdateUserAgent
 end
---
+++ app/controllers/concerns/update_user_agent.rb
@@ -0,0 +1,14 @@
+module UpdateUserAgent
+  extend ActiveSupport::Concern
+
+  included do
+    before_action :add_or_update_user_agent
+  end
+
+  def add_or_update_user_agent
+    # finds the user agent from database or creates one
+    user_agent = UserAgent.find_or_create_by(name: request.user_agent)
+    user_agent.increment!(:visits_count) # increments visits_count field by 1
+    user_agent.touch # updates updated_at field
+  end
+end
