f41e6a23465d0ed4abef6a113550f16e34b77710
Shambhavi Sachid <shambu1985@gmail.com>
2018-03-30 12:47:26 -0700

    Refactored user agents index view, moved table into a partial

--- app/controllers/user_agents_controller.rb
+++ app/controllers/user_agents_controller.rb
@@ -1,5 +1,8 @@
 class UserAgentsController < ApplicationController
   def index
     @user_agents = UserAgent.order(:updated_at)
+    respond_to do |format|
+      format.html { render :index }
+    end
   end
 end
