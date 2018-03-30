5e6d630a4bdcc1fd533eedea2d8c83f621394d9e
Shambhavi Sachid <shambu1985@gmail.com>
2018-03-30 13:03:52 -0700

    Added rpsec tests for pages and user_agents

--- spec/controllers/user_agents_controller_spec.rb
+++ spec/controllers/user_agents_controller_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe UserAgentsController, type: :controller do
+  describe "#index" do
+    let!(:user_agent) { FactoryGirl.create(:user_agent) }
+    before { get :index }
 
+    it "assigns @user_agents" do
+      expect(assigns(:user_agents)).to include(user_agent)
+    end
+
+    it "renders the index template" do
+      expect(response).to render_template(:index)
+    end
+
+    it "has a success http status" do
+      expect(response).to have_http_status(:success)
+    end
+  end
 end
