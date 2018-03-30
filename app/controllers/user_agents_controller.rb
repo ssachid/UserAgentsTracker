c38a98ac156ba2a62f416fdae7239b8cb24a5fb6
Shambhavi Sachid <shambu1985@gmail.com>
2018-03-30 12:48:33 -0700

    Implemented AJAX for the Refresh button

--- app/controllers/user_agents_controller.rb
+++ app/controllers/user_agents_controller.rb
@@ -3,6 +3,7 @@ class UserAgentsController < ApplicationController
     @user_agents = UserAgent.order(:updated_at)
     respond_to do |format|
       format.html { render :index }
+      format.js { render partial: "user_agents/list"}
     end
   end
 end
