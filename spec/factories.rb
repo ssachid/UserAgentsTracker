d3822f927cd782392480773f46e7b3f4e0508baf
Shambhavi Sachid <shambu1985@gmail.com>
2018-03-30 12:58:59 -0700

    Added spec files for pages and user_agents, add user_agent codeusing FactoryGirl

---
+++ spec/factories.rb
@@ -0,0 +1,5 @@
+FactoryGirl.define do
+  factory :user_agent do
+    name "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.162 Safari/537.36"
+  end
+end
